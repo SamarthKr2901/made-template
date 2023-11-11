8a33ae6096a97933b68804d45a47474068bcd435
Samarth__kumar <samarth.kumar2901@gmail.com>
2023-11-11 01:44:05 +0100

    Added Datatype conversion in Exercise1

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -13,8 +13,24 @@ sqlite_conn = "sqlite:///airports.sqlite"
 #SQLAlchemy engine
 engine = sa.create_engine(sqlite_conn)
 
-#Writing the data in the table "airports"
-data.to_sql("airports", engine, index=False, if_exists="replace")
+dtypes = {
+    "column_1": sa.types.INTEGER,
+    "column_2": sa.types.TEXT,
+    "column_3": sa.types.TEXT,
+    "column_4": sa.types.TEXT,
+    "column_5": sa.types.TEXT,
+    "column_6": sa.types.TEXT,
+    "column_7": sa.types.REAL,
+    "column_8": sa.types.REAL,
+    "column_9": sa.types.INTEGER,
+    "column_10": sa.types.REAL,
+    "column_11": sa.types.TEXT,
+    "column_12": sa.types.TEXT,
+    "geo_punkt": sa.types.TEXT
+}
+
+#Write the data in the table "airports"
+data.to_sql("airports", engine, index=False, if_exists="replace", dtype=dtypes)
 
 #Confirmation message
 print("Data pipeline completed successfully!")
