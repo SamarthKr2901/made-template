58d11f85b59726040c34f1cd12e48eb5c0880f1e
SamarthKr2901 <68009079+SamarthKr2901@users.noreply.github.com>
2023-12-20 15:59:18 +0100

    Updated tests.sh - added error code

--- project/tests.sh
+++ project/tests.sh
@@ -10,3 +10,10 @@ else
     echo "Error: Data pipeline failed to create output files."
     exit 1
 fi
+
+# Seems like the pipeline is not able to download the Kaggle twitch.csv database through the workflow, although it is working perfectly fine in my Local.
+# The csv fails to download which results in data.sqlite not being created, thus failing the pipeline and the test, returning the below error.  
+
+# FileNotFoundError: [Errno 2] No such file or directory: '/home/runner/work/made-template/made-template/../data/Twitch_game_data.csv'
+# Error: Data pipeline failed to create output files.
+# Error: Process completed with exit code 1.
