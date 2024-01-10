52698d353ab5f2220c663ef33c268fb64de6d433
Samarth__kumar <samarth.kumar2901@gmail.com>
2024-01-10 01:13:39 +0100

    Fixed Pipeline and updated Report

--- project/pipeline.py
+++ project/pipeline.py
@@ -6,9 +6,8 @@ from kaggle.api.kaggle_api_extended import KaggleApi
 
 # Dataset URLs
 twitch_dataset_url = 'rankirsh/evolution-of-top-games-on-twitch'
-covid19_dataset_url = 'https://covid19.who.int/WHO-COVID-19-global-data.csv'
+covid19_dataset_url = 'georgesaavedra/covid19-dataset'
 
-# Generating the data save path
 # Get current working Directory, which is the project/pipeline.py
 current_directory = os.getcwd()
 # Going 1 step back and into the data folder
@@ -17,27 +16,46 @@ data_folder = os.path.join(current_directory, '..', 'data')
 if not os.path.exists(data_folder):
     os.makedirs(data_folder)
 
+print("authenticating with Kaggle")
+
 api = KaggleApi()
 api.authenticate()
 
-# Downloading Dataset
+print("authenticating Successfully")
+
+# Downloading Twitch Dataset
 try:
     api.dataset_download_files(twitch_dataset_url, path=data_folder, unzip=True)
 except KeyError:
-    print("Error: 'Content-Length' header not found in the response from the Kaggle API.")
+    print("Error: 'Content-Length' header not found in the response from the Kaggle API for Twitch dataset.")
 
-# Reading Both Datasets
-csv_file_path = os.path.join(data_folder, 'Twitch_game_data.csv')
-twitch_data = pd.read_csv(csv_file_path, sep=",", lineterminator="\n", encoding='cp1252')
+print("Twitch Data Downloaded")
 
-# Displaying Downloaded Files - COVID dataset file is not downloaded/displayed but directly added to the SQLite database
+# Downloading COVID-19 Dataset
+try:
+    api.dataset_download_files(covid19_dataset_url, path=data_folder, unzip=True)
+except KeyError:
+    print("Error: 'Content-Length' header not found in the response from the Kaggle API for COVID-19 dataset.")
+
+print("covid data downloaded")
+
+# Displaying Downloaded Files
 files = os.listdir(data_folder)
 print("Downloaded files:", files)
 
-# Reading WHO COVID-19 Global Data
-covid19_data = pd.read_csv(covid19_dataset_url)
+# Reading Twitch Dataset
+csv_file_path_twitch = os.path.join(data_folder, 'Twitch_game_data.csv')
+print("Reading twitch data from"+csv_file_path_twitch)
+twitch_data = pd.read_csv(csv_file_path_twitch, sep=",", lineterminator="\n", encoding='cp1252')
 
-# Updating the save path for the SQLite database file to be also in the Data folder
+# Reading COVID-19 Dataset
+csv_file_path_covid19 = os.path.join(data_folder, 'owid-covid-data.csv')
+print("Reading covid data from"+csv_file_path_covid19)
+covid_data = pd.read_csv(csv_file_path_covid19, sep=",", lineterminator="\n", encoding='cp1252')
+
+print("Completed Reading Data")
+
+# Updating the save path for the SQLite database file to also be in the Data folder
 sqlite_file_path = os.path.join(data_folder, 'data.sqlite')
 
 # SQLite connection
@@ -46,11 +64,12 @@ sqlite_conn = f"sqlite:///{sqlite_file_path}"
 # SQLAlchemy engine
 engine = sa.create_engine(sqlite_conn)
 
-# Write the data in the table "airports"
+print("Established Connection with SQLite")
+
+# Write the data to the respective tables
 twitch_data.to_sql("twitch_data", engine, index=False, if_exists="replace")
+covid_data.to_sql("covid_data", engine, index=False, if_exists="replace")
 
-# Write the COVID-19 data to the table "covid19_data"
-covid19_data.to_sql("covid19_data", engine, index=False, if_exists="replace")
+print("Saved Data to SQLite Database")
 
-# Confirmation message
 print("Data pipeline completed successfully!")
